862decd36a02225099c408477615e094e4ae941c
cfcromn <s25058@gsm.hs.kr>
2025-06-24 19:29:14 +0900

    java-study

--- src/scanner/ex/scannerWhileEX4.java
+++ src/scanner/ex/scannerWhileEX4.java
@@ -7,6 +7,7 @@ public class scannerWhileEX4 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int totalCost = 0;
+
         while (true) {
             System.out.println("1: 상품 입력, 2: 결제, 3: 프로그램 종료");
             int option = input.nextInt();
