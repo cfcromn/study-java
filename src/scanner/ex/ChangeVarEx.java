f4395b725fc5ef3cc7e866021ab3b0f06b68bda1
cfcromn <s25058@gsm.hs.kr>
2025-06-24 19:28:16 +0900

    java-study

---
+++ src/scanner/ex/ChangeVarEx.java
@@ -0,0 +1,17 @@
+package scanner.ex;
+
+public class ChangeVarEx {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int temp; //a의 값 10
+
+        temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
